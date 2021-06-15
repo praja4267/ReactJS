1463bcb4730b9d030e26e535406102ad8bf04824
Rajasekhar <praja4267@gmail.com>
2021-06-15 22:07:27 +0530

    Corrected code

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ function App() {
       <br/>
       <h1>Hello World</h1>
       <label htmlFor="name"> User Name: </label>
-      <input type="text" id="fName" placeholder="RajaSekhar" defaultValue="Raja"/><br/>
-      <span>Name</span><br/>
-      <span>Phone</span>
+      <input type="text" id="fName" placeholder="RajaSekhar" defaultValue="Raja"/><br/><br/ >
+      <label htmlFor="user_password"> Password: </label>
+      <input type="text" id="user_password" placeholder="RajaSekhar" defaultValue="1234"/><br/><br/ >
+      <span>Phone</span><br/><br/>
+
+      <label htmlFor="username">Username: </label> <br/>
+      <input id="username" type="text"/><br/><br/>
+      <label htmlFor="password">Password: </label><br/>
+      <input id="password" type="password"/><br/><br/>
     </div>
   );
 }
